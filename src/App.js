4b92c89b3ec16290b238ab2320fff4384c265623
leandroandrade <leandro.andrade2588@gmail.com>
2020-09-02 13:30:01 -0300

    add persistence storage

--- src/App.js
+++ src/App.js
@@ -19,12 +19,31 @@ class App extends React.Component {
         return this.getLetter(alphabet, quantity, [...data, alphabet.charAt(Math.floor(Math.random() * 26))]);
     }
 
+    setData(letter, letters = []) {
+        this.setState({ letter });
+
+        letters.push(letter);
+        window.sessionStorage.setItem('letters', letters.join());
+    }
 
     getRandomLetter() {
         const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
         const letter = this.getLetter(alphabet, 1);
 
-        this.setState({ letter });
+        const strings = window.sessionStorage.getItem('letters');
+        if (!strings) {
+            return this.setData(letter);
+        }
+
+        const letters = strings.split(',');
+        if (letters.length === alphabet.length) {
+            window.sessionStorage.clear();
+        }
+
+        if (letters.includes(letter)) {
+            return this.getRandomLetter();
+        }
+        this.setData(letter, letters);
     }
 
     render() {
